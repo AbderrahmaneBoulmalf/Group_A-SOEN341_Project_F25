6158d1694288a598cde021d054d0779a08fa1cdb
Sami Erafii <sami.erafii@gmail.com>
2025-10-01 20:04:21 -0400

    Added database module with mysql connection pool. Added docker-compose file for phpmyadmin.

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,3 +1,13 @@
 import express from "express";
+import cors from "cors";
+import db from "./db.js";
 
 const app = express();
+console.log("Database module imported:", db ? "success" : "failed");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // React URL
+    credentials: true,
+  })
+);
