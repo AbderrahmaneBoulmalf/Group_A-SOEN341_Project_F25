2f372c20231dda36b40905deeb6cb388b5a308c4
Sami Erafii <sami.erafii@gmail.com>
2025-10-07 18:38:36 -0400

    Moved Register.tsx into pages folder and included it into App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import React from "react";
+import Register from "./pages/Register";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
       </Routes>
     </Router>
   );
