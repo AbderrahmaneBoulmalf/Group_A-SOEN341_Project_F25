766734f562e5014f61b47bbd165a494dd11bb835
marialagh <marialaghmari@hotmail.com>
2025-10-09 12:02:45 -0400

    #20-21 - [~] Backend: removed commented code and cleaned up events.ts

--- Backend/src/events/events.ts
+++ Backend/src/events/events.ts
@@ -1,25 +1,5 @@
-/*import db from "../db.js";
-import { Request, Response } from "express";
-
-const getEvents = (req: Request, res: Response) => {
-  const sql = "SELECT * FROM events";
-  db.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to fetch events" });
-    } else {
-      res.json(results);
-    }
-  });
-};
-
-const events = {
-  getEvents,
-};
-
-export default events;
-*/
 import type { Request, Response } from "express";
-// path is correct because this file is /src/events/events.ts and db.ts is /src/db.ts
+
 import db from "../db.js";
 
 
