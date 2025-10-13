0dd032f1c58560143b95b0e29fef6fa0c76aa3f9
RafaelL0311 <rwlenz0311@gmail.com>
2025-10-12 21:17:45 -0400

    Delete routes directory

--- routes/passAuth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { issuePass } from "../middleware/issuePass.js";
-
-const router = Router();
-
-router.post("/issue-pass", issuePass);
-
-export default router;
