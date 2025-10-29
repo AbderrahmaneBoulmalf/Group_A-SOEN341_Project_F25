ba41a062b60df8b50d9566d5da29619a6f143fe0
Sami Erafii <sami.erafii@gmail.com>
2025-10-29 12:14:43 -0400

    Integrated CI pipeline with github actions. Configured jest to run tests in Backend/src/tests (#80)

---
+++ Backend/src/tests/example.test.ts
@@ -0,0 +1,9 @@
+//Don't write all of your tests here. This is just an example test file.
+//You should create separate test files for different modules or functionalities.
+//End with .test.ts or .spec.ts extension.
+
+describe("Sample test", () => {
+  it("works", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
