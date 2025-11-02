53502c9d1c400852e712cf29fc031f1107df836d
RafaelL0311 <rwlenz0311@gmail.com>
2025-11-02 01:07:11 -0500

    Add files via upload

--- Frontend/src/components/Dashboards/Manager/Elements/Settings.tsx
+++ Frontend/src/components/Dashboards/Manager/Elements/Settings.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteAccount from "../EditAccount/DeleteAccount";
+import EditInfo from "../EditAccount/EditInfo";
+import UpdatePassword from "../EditAccount/UpdatePassword";
 
-const ManagerSettings: React.FC = () => {
+const Settings: React.FC = () => {
+  const [error, setError] = useState<boolean>(false);
+  const [deleteModal, setDeleteModal] = useState<boolean>(false);
+
+  const AccountDeletion: React.FC = () => {
+    return (
+      <div className="shadow-md rounded-xl border border-gray-200 bg-white/60 backdrop-blur-sm p-10 pl-4 w-full">
+        <h1 className="mb-2 text-base font-semibold">Close your account</h1>
+        <p className="text-sm">
+          Closing your account is permanent and cannot be undone. All your data,
+          including saved preferences and history, will be deleted. If you're
+          sure you want to proceed, please confirm below.
+        </p>
+        <br />
+        <DeleteAccount modal={deleteModal} setModal={setDeleteModal} />
+      </div>
+    );
+  };
+
   return (
-    <div className="ml-4 mb-10 mr-4 mt-2 flex items-center justify-center h-[98%]">
-      <div className="mt-4 flex flex-col gap-y-6">New feature coming soon</div>
+    <div className="ml-4 mb-10 mr-4 mt-2 flex items-center flex-col h-[98%]">
+      {error ? (
+        <div className="flex h-40 items-center justify-center text-center text-red-400">
+          <p>Error fetching data. Please try again later.</p>
+        </div>
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center space-y-10 mt-10 mb-10">
+          <div className="w-full flex-col flex items-center justify-center shadow-md rounded-xl border border-gray-200 bg-white/60 backdrop-blur-sm p-8 pl-4">
+            <EditInfo setError={setError} />
+            <UpdatePassword />
+          </div>
+          <AccountDeletion />
+        </div>
+      )}
     </div>
   );
 };
 
-export default ManagerSettings;
+export default Settings;
