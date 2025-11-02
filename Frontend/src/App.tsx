776a835c3581e342c1b8c213acea8ec3bec07105
RafaelL0311 <rwlenz0311@gmail.com>
2025-11-02 01:00:16 -0500

    Add files via upload

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,47 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Events from "./pages/Events";
-import EventDetails from "./pages/EventDetails";
+import React from "react";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
-
 import ProtectedRoutes from "./Protected/ProtectedRoutes";
 import Unauthorized from "./Protected/Unauthorized";
-
-// Student
 import Layout from "./pages/StudentDashboard/Layout";
 import SavedEvents from "./components/Dashboards/Student/Elements/SavedEvents";
 import Calendar from "./components/Dashboards/Student/Elements/Calendar";
 import Tickets from "./components/Dashboards/Student/Elements/Tickets";
 import Settings from "./components/Dashboards/Student/Elements/Settings";
-import QRCodeGen from "./pages/StudentDashboard/QRCodeGen";
-import QRCodeReader from "./pages/QRReader";
-import Payment from "@/pages/Payment";
-
-// Manager
+import Register from "./pages/Register";
+import EventDetails from "./pages/EventDetails";
 import ManagerLayout from "./pages/EventsDashboard/Layout";
 import MyEvents from "./components/Dashboards/Manager/Elements/MyEvents";
 import CreateEvents from "./components/Dashboards/Manager/Elements/CreateEvents";
 import ManagerSettings from "./components/Dashboards/Manager/Elements/Settings";
-import EventAnalytics from "./components/Dashboards/Manager/Elements/EventAnalytics";
+import QRCodeGen from "./pages/StudentDashboard/QRCodeGen";
+import QRCodeReader from "./pages/QRReader";
+import Payment from "@/pages/Payment";
 
-// Admin
 import AdminLayout from "./pages/AdminDashboard/Layout";
 import ApprovalsPage from "./pages/AdminDashboard/ApprovalsPage";
 import ModerationPage from "./pages/AdminDashboard/ModerationPage";
 import AnalyticsPage from "./pages/AdminDashboard/AnalyticsPage";
 import OrgsRolesPage from "./pages/AdminDashboard/OrgsRolesPage";
-import AdminSettings from "./pages/AdminDashboard/SettingsPage";
+import AdminSettings from "./components/Dashboards/Admin/Elements/Settings";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
@@ -52,8 +43,6 @@ const App: React.FC = () => {
         <Route path="qr/:eventId" element={<QRCodeGen />} />
         <Route path="qrreader" element={<QRCodeReader />} />
         <Route path="/payment" element={<Payment />} />
-
-        {/* Protected Admin Routes */}
         <Route element={<ProtectedRoutes role="admin" />}>
           <Route path="admin" element={<AdminLayout />}>
             <Route index element={<ApprovalsPage />} />
@@ -64,19 +53,14 @@ const App: React.FC = () => {
             <Route path="settings" element={<AdminSettings />} />
           </Route>
         </Route>
-
-        {/* Protected Manager Routes */}
         <Route element={<ProtectedRoutes role="manager" />}>
           <Route path="manager" element={<ManagerLayout />}>
             <Route index element={<MyEvents />} />
             <Route path="my-events" element={<MyEvents />} />
             <Route path="create-events" element={<CreateEvents />} />
             <Route path="settings" element={<ManagerSettings />} />
-            <Route path="event/:id" element={<EventAnalytics />} />
           </Route>
         </Route>
-
-        {/* Protected Student Routes */}
         <Route element={<ProtectedRoutes role="student" />}>
           <Route path="student" element={<Layout />}>
             <Route index element={<SavedEvents />} />
@@ -86,8 +70,6 @@ const App: React.FC = () => {
             <Route path="settings" element={<Settings />} />
           </Route>
         </Route>
-
-        {/* Unauthorized Route */}
         <Route path="/unauthorized" element={<Unauthorized />} />
       </Routes>
     </Router>
